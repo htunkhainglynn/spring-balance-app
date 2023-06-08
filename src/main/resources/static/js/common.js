55bb83c2b3bb23f978c2467018d7a0a0e812a82a
Htun Khaing Linn <htunkhainglynn13@gmail.com>
2023-06-08 23:52:44 +0630

    add dynamic user search

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,5 +1,13 @@
+import bootstrap from 'bootstrap';
+
 document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('signoutLink').addEventListener('click', () => {
 		document.getElementById('signoutForm').submit();
 	})
+	
+	Array.from(document.getElementsByClassName('userStatusChangeLink'))
+		.forEach((link) => link.addEventListener('click', () => {
+			const dialog = new bootstrap.Modal("#userStatusChangeLink");
+			dialog.show();
+		}))
 })
